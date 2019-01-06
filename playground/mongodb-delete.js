7deb114709bffe20142c6d730299f84cd52c9f11
Holden B <holden.bucher.92@gmail.com>
2019-01-05 20:33:55 -0600

    delete db entries

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,41 @@
+// const MongoClient = require("mongodb").MongoClient;
+const { MongoClient, ObjectID } = require("mongodb");
+
+MongoClient.connect(
+  "mongodb://localhost:27017/TodoApp",
+  { useNewUrlParser: true },
+  (err, client) => {
+    if (err) {
+      return console.log("Unable to connect to MongoDB server");
+    }
+    console.log("Connected to MongoDB server");
+    const db = client.db("TodoApp");
+
+    //deleteMany
+    // db.collection("Todos")
+    //   .deleteMany({ text: "Eat lunch" })
+    //   .then(res => console.log(res));
+
+    //deleteOne
+    // db.collection("Todos")
+    //   .deleteOne({ text: "Eat lunch" })
+    //   .then(res => console.log(res));
+
+    //findOneAndDelete
+    // db.collection("Todos")
+    //   .findOneAndDelete({ completed: false })
+    //   .then(res => console.log(res));
+
+    //PRACTICE PROBBLEMS
+
+    // db.collection("Users")
+    //   .deleteMany({ name: "Holden" })
+    //   .then(res => console.log(res));
+
+    // db.collection("Users")
+    //   .deleteOne({ _id: new ObjectID("5c314ac994b174b6efb8fb73") })
+    //   .then(res => console.log(res));
+
+    // client.close();
+  }
+);
